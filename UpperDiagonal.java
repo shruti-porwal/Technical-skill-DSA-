11e01605c3bc4e5a6acc7ab1978f0169e0766f07
shruti-porwal <porwalshruti230@gmail.com>
2026-02-11 23:15:30 +0530

    commit

---
+++ UpperDiagonal.java
@@ -0,0 +1,19 @@
+public class UpperDiagonal{
+    public static void main(String[] args) {
+        int[][] arr = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+        int sum = 0;
+        for(int i = 0;i<arr.length;i++){
+            for(int j = 0;j<arr.length;j++){
+                if(i<j){
+                    System.out.println(arr[i][j]);
+                    sum += arr[i][j];
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
