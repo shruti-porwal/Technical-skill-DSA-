067c40c461b018ec6a423796606f24346dc1a1b2
shruti-porwal <porwalshruti230@gmail.com>
2026-02-11 23:27:38 +0530

    commit

---
+++ ColSum.java
@@ -0,0 +1,16 @@
+public class ColSum {
+    public static void main(String[] args) {
+        int[][] arr = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9},
+        };
+        for(int i = 0;i<arr.length;i++){
+            int sum = 0;
+            for(int j = 0;j<arr.length;j++){
+                sum += arr[j][i];
+            }
+            System.out.println(sum);
+        }
+    }
+}
